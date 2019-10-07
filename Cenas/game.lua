ed433e540a9ca62164777cf0c0228c18979cb3bb
Nailson Oliveira <eryfernandes2@gmail.com>
2019-10-07 12:16:32 -0300

    Criação de colisão e aumento da dificuldade de jogo

--- Cenas/game.lua
+++ Cenas/game.lua
@@ -1,11 +1,9 @@
 local composer = require( "composer" )
- 
 local scene = composer.newScene()
-
+local base = require( "base" )
 --Fisica
 local physics = require( "physics" )
 physics.start()
-physics.setGravity( 0, 2)
 -- physics.setDrawMode("hybrid")
 
 --Grupos
@@ -21,32 +19,38 @@ local lixeiraAzul
 local lixeiraVermelha
 local lixeiraAmarela
 local lixeiraVerde
-local pontuacao
-
+local caixaPontuacao
+local pontos
+local vidas
 local gameLoopTimer
+local gameLoopDificuldade
+local velocidadeQueda
+local velocidadeGerar
 
 --Tabelas
 local trashTable = {}
 
+
+--Inicialização de variaveis
+pontos = 0
+vidas = 3
+velocidadeGerar = 3000
+velocidadeQueda = 1
+
+--Inicialização da física
+physics.setGravity( 0, 1 )
+
 --funções
 local function createTrash()
 
-    local tipoLixo = {}
-    tipoLixo[1] =  display.newImageRect( "Imagens/garrafa-vidro.png", 20, 40)
-    tipoLixo[2] =  display.newImageRect( "Imagens/lata.png", 30, 40)
-    tipoLixo[3] =  display.newImageRect( "Imagens/vidro.png", 30, 40)
-    tipoLixo[4] =  display.newImageRect( "Imagens/papel.png", 40, 40)
-    tipoLixo[1].x = -200
-    tipoLixo[2].x = -200
-    tipoLixo[3].x = -200
-    tipoLixo[4].x = -200
-    
-    local newTrash = tipoLixo[math.random( 1,4 )]
+    local this = math.random(1,9)
+
+    local newTrash = display.newImageRect(mainGroup, base.tipoLixo[this].img, base.tipoLixo[this].x, base.tipoLixo[this].y)
     physics.addBody( newTrash, "dynamic", { radius=30, bounce=0.0 } )
     newTrash.myName = "trash"
     newTrash.x = math.random( 20, display.contentWidth-20 )
     newTrash.y = -60
-    newTrash:setLinearVelocity( 0, math.random( 2,4 ) )
+    newTrash:setLinearVelocity( 0, velocidadeQueda )
     newTrash:applyTorque( math.random( -6,6 ) )
     table.insert( trashTable, newTrash )
 
@@ -63,11 +67,29 @@ local function createTrash()
         end  
     end
     newTrash:addEventListener("touch", moverLixo)
+
+    local function colisaoLixo(self, event)
+        local obj1 = event.target
+        local obj2 = event.other
+
+        if(obj2.myName == base.tipoLixo[this].tipo)
+        then
+            display.remove(obj1)
+            pontos = pontos + 10
+            for i = #trashTable, 1, -1 do
+                if ( trashTable[i] == obj1) then
+                    table.remove( trashTable, i)
+                    break
+                end
+            end
+        end
+    end
+    newTrash.collision = colisaoLixo
+    newTrash:addEventListener("collision")
 end
 
 
 local function gameLoop()
-
 	-- Create new trash
     createTrash()
 
@@ -83,7 +105,30 @@ local function gameLoop()
 			display.remove( thisTrash )
 			table.remove( trashTable, i )
 		end
-	end
+    end
+    
+
+end
+
+local function aumentarDificuldade()
+    if(velocidadeQueda < 1000)
+    then
+        velocidadeQueda = velocidadeQueda + 10
+        print(velocidadeQueda)
+    end
+end
+
+local function aumentarVelocidadeQueda(event)
+    if(velocidadeQueda < 100)then
+        gameLoopTimer = timer.performWithDelay(3000, gameLoop, 0 )
+        print("gerando a cada 3 segundos")
+    elseif(velocidadeQueda > 100 and velocidadeQueda < 200)then
+        gameLoopTimer = timer.performWithDelay(2000, gameLoop, 0 )
+        print("gerando a cada 2 segundos")
+    elseif(velocidadeQueda > 200)then
+        gameLoopTimer = timer.performWithDelay(1000, gameLoop, 0 )
+        print("gerando a cada 1 segundos")
+    end
 end
 
 -- create()
@@ -98,30 +143,36 @@ function scene:create( event )
 
     plataforma = display.newImageRect(mainGroup, "Imagens/layer-5.png" , 450, 80)
     plataforma.x = display.contentCenterX
-    plataforma.y = display.contentCenterY+280
+    plataforma.y = display.contentHeight+40
 	physics.addBody(plataforma, "static")
 	
 	lixeiraAzul = display.newImageRect(mainGroup, "Imagens/lixeira-azul-papel.png" , 60, 80)
     lixeiraAzul.x = display.contentCenterX-120
 	lixeiraAzul.y = display.contentCenterY+210
-	physics.addBody(lixeiraAzul, "static")
+    physics.addBody(lixeiraAzul, "static")
+    lixeiraAzul.myName = "Papel"
 	
 	lixeiraVermelha = display.newImageRect(mainGroup, "Imagens/lixeira-vermelho-plastico.png" , 60, 80)
     lixeiraVermelha.x = display.contentCenterX-40
 	lixeiraVermelha.y = display.contentCenterY+210
-	physics.addBody(lixeiraVermelha, "static")
+    physics.addBody(lixeiraVermelha, "static")
+    lixeiraVermelha.myName = "Plastico"
 
 	lixeiraAmarela = display.newImageRect(mainGroup, "Imagens/lixeira-amarela-metal.png" , 60, 80)
     lixeiraAmarela.x = display.contentCenterX+40
 	lixeiraAmarela.y = display.contentCenterY+210
-	physics.addBody(lixeiraAmarela, "static")
+    physics.addBody(lixeiraAmarela, "static")
+    lixeiraAmarela.myName = "Metal"
 
 	lixeiraVerde = display.newImageRect(mainGroup, "Imagens/lixeira-verde-vidro.png" , 60, 80)
     lixeiraVerde.x = display.contentCenterX+120
 	lixeiraVerde.y = display.contentCenterY+210
     physics.addBody(lixeiraVerde, "static")
+    lixeiraVerde.myName = "Vidro"
     
-    
+    caixaPontuacao = display.newImageRect(uiGroup, "Imagens/Caixa-pontuacao.png" , 110, 40)
+    caixaPontuacao.x = display.contentWidth-55
+
 
     sceneGroup:insert(backGroup)
     sceneGroup:insert(mainGroup)
@@ -140,7 +191,8 @@ function scene:show( event )
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
         physics.start()
-        gameLoopTimer = timer.performWithDelay( 2000, gameLoop, 0 )
+        gameLoopDificuldade = timer.performWithDelay(10000, aumentarDificuldade, 0 )
+        gameLoopTimer = timer.performWithDelay(3000, gameLoop, 0 )
     end
 end
  
---
+++ base.lua
@@ -0,0 +1,58 @@
+local base = {}
+base.tipoLixo = {}
+
+base.tipoLixo[1] = {}
+base.tipoLixo[1].img = "Imagens/lata.png"
+base.tipoLixo[1].x = 40
+base.tipoLixo[1].y = 40
+base.tipoLixo[1].tipo = "Metal"
+
+base.tipoLixo[2] = {}
+base.tipoLixo[2].img = "Imagens/papel.png"
+base.tipoLixo[2].x = 40
+base.tipoLixo[2].y = 40
+base.tipoLixo[2].tipo = "Papel"
+
+base.tipoLixo[3] = {}
+base.tipoLixo[3].img = "Imagens/garrafa-vidro.png"
+base.tipoLixo[3].x = 25
+base.tipoLixo[3].y = 50
+base.tipoLixo[3].tipo = "Vidro"
+
+base.tipoLixo[4] = {}
+base.tipoLixo[4].img = "Imagens/taca.png"
+base.tipoLixo[4].x = 40
+base.tipoLixo[4].y = 40
+base.tipoLixo[4].tipo = "Vidro"
+
+base.tipoLixo[5] = {}
+base.tipoLixo[5].img = "Imagens/garrafa-plastico.png"
+base.tipoLixo[5].x = 40
+base.tipoLixo[5].y = 40
+base.tipoLixo[5].tipo = "Plastico"
+
+base.tipoLixo[6] = {}
+base.tipoLixo[6].img = "Imagens/caixa-papelao.png"
+base.tipoLixo[6].x = 40
+base.tipoLixo[6].y = 40
+base.tipoLixo[6].tipo = "Papel"
+
+base.tipoLixo[7] = {}
+base.tipoLixo[7].img = "Imagens/livro.png"
+base.tipoLixo[7].x = 40
+base.tipoLixo[7].y = 40
+base.tipoLixo[7].tipo = "Papel"
+
+base.tipoLixo[8] = {}
+base.tipoLixo[8].img = "Imagens/sacola-plastica.png"
+base.tipoLixo[8].x = 40
+base.tipoLixo[8].y = 40
+base.tipoLixo[8].tipo = "Plastico"
+
+base.tipoLixo[9] = {}
+base.tipoLixo[9].img = "Imagens/garrafao.png"
+base.tipoLixo[9].x = 40
+base.tipoLixo[9].y = 40
+base.tipoLixo[9].tipo = "Plastico"
+
+return base
